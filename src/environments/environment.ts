bb4a0d32aec55fce7131cef44e6cea287266ace1
Abhishek-1705 <am9893073458@gmail.com>
2022-03-26 13:49:11 +0530

    made sign and login page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,19 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { FirebaseError } from "firebase/app";
+
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+
+      apiKey: "AIzaSyDhnTLPS0nTmZE_A_awXPe-auWXKFB30vU",
+      authDomain: "prayas-test-b4ffb.firebaseapp.com",
+      projectId: "prayas-test-b4ffb",
+      storageBucket: "prayas-test-b4ffb.appspot.com",
+      messagingSenderId: "711882854817",
+      appId: "1:711882854817:web:f58a08a519d7a2ca69fbe0"
+    }
 };
 
 /*
